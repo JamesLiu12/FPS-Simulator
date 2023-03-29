4690e758501846fc783e1d84d3f80e42f8a3565a
JamesLiu12 <39219648+JamesLiu12@users.noreply.github.com>
2023-03-29 18:26:27 +0800

    Fix a bug in OBJ_CONVERTER
    Obj might contain the quadrilateral.
    Add the converter of quadrilateral

--- obj_converter/Obj.hs
+++ obj_converter/Obj.hs
@@ -22,13 +22,36 @@ getVertices =
         getVertex [_, xRaw, yRaw, zRaw] = (read xRaw, read yRaw, read zRaw)
 
 getFaces :: [[String]] -> [Triangle]
-getFaces = 
-    filter ((=="f") . head) >>>
+getFaces x = getFaces3 x ++ getFaces4_1 x ++ getFaces4_2 x
+
+getFaces3 :: [[String]] -> [Triangle]
+getFaces3 =
+    filter (\x -> head x == "f" && length x == 4) >>>
     map getFace
     where
         getFace [_, v1, v2, v3] = (getVertexIndex v1, getVertexIndex v2, getVertexIndex v3)
         getVertexIndex :: String -> Int
         getVertexIndex = read . takeWhile (/='/')
+
+getFaces4_1 :: [[String]] -> [Triangle] 
+getFaces4_1 =
+    filter (\x -> head x == "f" && length x == 5) >>>
+    map getFace
+    where
+        getFace [_, v1, v2, v3, v4] = (getVertexIndex v1, getVertexIndex v2, getVertexIndex v3)
+        getVertexIndex :: String -> Int
+        getVertexIndex = read . takeWhile (/='/')
+
+getFaces4_2 :: [[String]] -> [Triangle] 
+getFaces4_2 =
+    filter (\x -> head x == "f" && length x == 5) >>>
+    map getFace
+    where
+        getFace [_, v1, v2, v3, v4] = (getVertexIndex v1, getVertexIndex v4, getVertexIndex v3)
+        getVertexIndex :: String -> Int
+        getVertexIndex = read . takeWhile (/='/')
+    
+    
 
 parseObj :: String -> Obj
 parseObj = 
