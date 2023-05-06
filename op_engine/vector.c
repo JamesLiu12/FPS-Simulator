ba624af30fcc65cb2edaba39bdada403b38ff7d0
fauojrw <125647562+fauojrw@users.noreply.github.com>
2023-05-06 23:50:53 +0800

    added comments to functions in vector.c and vector.h
    added comments to functions in vector.c and vector.h

--- op_engine/vector.c
+++ op_engine/vector.c
@@ -4,79 +4,61 @@
 #include "math.h"
 #include "../util/util.h"
 
-// copies the data from one vector to another
-// input: a pointer to the vector being copied from, a pointer to the vector being copied to
-// output: void
+
 void Vector3_Copy(struct Vector3 *from, struct Vector3 *to) {
     to->x = from->x;
     to->y = from->y;
     to->z = from->z;
 }
 
-// assigns values to a 3D vector
-// input: a pointer to a vector struct, three doubles, x, y, z representing the components of the vector
-// output: void
+
 void Vector3_Set(struct Vector3 *v, double x, double y, double z){
     v->x = x;
     v->y = y;
     v->z = z;
 }
 
-// creates a new 3D vector
-// input: 3 doubles, x, y, z, representing the components of the vecotr
-// output: a pointer to a vector struct
+
 struct Vector3* Vector3_New(double x, double y, double z){
     struct Vector3 *vector = malloc(sizeof(struct Vector3));
     Vector3_Set(vector, x, y, z);
     return vector;
 }
 
-// computes vector sum of two vectors
-// input: two pointers to vector struct
-// output: void
+
 void Vector3_Add(struct Vector3 *v1, struct Vector3 *v2){
     v1->x += v2->x;
     v1->y += v2->y;
     v1->z += v2->z;
 }
 
-// computes the vector subtraction of two vectors
-// input: two pointers to vector struct
-// output: void
+
 void Vector3_Subtract(struct Vector3 *v1, struct Vector3 *v2) {
     v1->x -= v2->x;
     v1->y -= v2->y;
     v1->z -= v2->z;
 }
 
-// scales the vector
-// input: a pointer to a vector struct, a double factor
-// output: void
+
 void Vector3_Scale(struct Vector3 *v, double factor){
     v->x *= factor;
     v->y *= factor;
     v->z *= factor;
 }
 
-// changes the vector into a unit vector
-// input: a pointer to a vector struct
-// output: void
+
 void Vector3_Normalize(struct Vector3 *v){
     v->x /= sqrt(Vector3_MagnitudeSq(v));
     v->y /= sqrt(Vector3_MagnitudeSq(v));
     v->z /= sqrt(Vector3_MagnitudeSq(v));
 }
 
-// returns the square of the magnitude of a vector
-// input: a pointer to a vector
-// output: the square of the magniture of the vector as a double 
+
 double Vector3_MagnitudeSq(struct Vector3 *v){
     return v->x * v->x + v->y * v->y + v->z * v->z;
 }
 
-// returns the magnitude of a vector
-// input: a pointer to a vector
-// output: the magniture of the vector as a double
+
 double Vector3_Magnitude(struct Vector3 *v){
     return sqrt(Vector3_MagnitudeSq(v));
 }
@@ -86,62 +68,46 @@ double Vector3_CrossProduct2D(struct Vector3 *v1, struct Vector3 *v2){
     return v1->x * v2->y - v2->x * v1->y;
 }
 
-// returns the square of the magnitude of a vector considering only the x and y components
-// input: a pointer to a vector
-// output: the square of the magniture of the vector as a double 
+
 double Vector3_DistanceSq2D(struct Vector3 *v1, struct Vector3 *v2){
     return (v1->x - v2->x) * (v1->x - v2->x) + (v1->y - v2->y) * (v1->y - v2->y);
 }
 
-// returns the magnitude of a vector considering only the x and y components
-// input: a pointer to a vector
-// output: the magniture of the vector as a double
+
 double Vector3_Distance2D(struct Vector3 *v1, struct Vector3 *v2){
     return sqrt(Vector3_DistanceSq2D(v1, v2));
 }
 
-// computes the cross product of two vectors and assigns it to a vector struct
-// input: two pointers to vectors, a pointer to the resultant vector
-// output: void
+
 void Vector3_CrossProduct3D(struct Vector3 *v1, struct Vector3 *v2, struct Vector3 *result){
     result->x = v1->y * v2->z - v1->z * v2->y;
     result->y = -(v1->x * v2->z - v1->z * v2->x);
     result->z = v1->x * v2->y - v1->y * v2->x;
 }
 
-// computes the dot product of two vectors
-// input: two pointers to vectors
-// output: the dot product of the two vectors as a double
+
 double Vector3_DotProduct3D(struct Vector3 *v1, struct Vector3 *v2){
     return v1->x * v2->x + v1->y * v2->y + v1->z * v2->z;
 }
 
-// returns the squared distance of two vectors
-// input: two pointers to vectors
-// output: the square of the distance of two vectors vector as a double 
+
 double Vector3_DistanceSq3D(struct Vector3 *v1, struct Vector3 *v2){
     return (v1->x - v2->x) * (v1->x - v2->x) + (v1->y - v2->y) * (v1->y - v2->y) + (v1->z - v2->z) * (v1->z - v2->z);
 }
 
-// returns the distance of two vectors
-// input: two pointers to vectors
-// output: the distance of two vectors vector as a double 
+
 double Vector3_Distance3D(struct Vector3 *v1, struct Vector3 *v2){
     return sqrt(Vector3_DistanceSq3D(v1, v2));
 }
 
-// judges whether two vectors are equal
-// input: two pointers to vectors
-// output: an integer similar to a boolean variable representing whether the vectors are equal
+
 int Vector3_Equal(struct Vector3 *v1, struct Vector3 *v2){
     return double_Equal(v1->x, v2->x)
            && double_Equal(v1->y, v2->y)
            && double_Equal(v1->z, v2->z);
 }
 
-// multiplies two vectors by components
-// input: one vector pointer that is multiplied, another vector pointer used to multiply with the first one
-// output: void
+
 void Vector3_Multiply(struct Vector3 *v1, struct Vector3 *v2){
     v1->x *= v2->x;
     v1->y *= v2->y;
--- op_engine/vector.h
+++ op_engine/vector.h
@@ -13,30 +13,61 @@ struct Vector3 {
 Basic operations for Vector3
 */
 
-//The setter for Vector3
+// assigns values to a 3D vector
+// input: a pointer to a vector struct, three doubles, x, y, z representing the components of the vector
+// output: void
 void Vector3_Set(struct Vector3 *v, double x, double y, double z);
 
 //Copy the value in "from" to "to"
+// input: a pointer to the vector being copied from, a pointer to the vector being copied to
+// output: void
 void Vector3_Copy(struct Vector3 *from, struct Vector3 *to);
 
-//The newer for Vector3
+// creates a new 3D vector
+// input: 3 doubles, x, y, z, representing the components of the vecotr
+// output: a pointer to a vector struct
 struct Vector3* Vector3_New(double x, double y, double z);
 
 //The basic operations for Vector3
+
+// computes vector sum of two vectors
+// input: two pointers to vector struct
+// output: void
 void Vector3_Add(struct Vector3 *v1, struct Vector3 *v2);
+
+// computes the vector subtraction of two vectors
+// input: two pointers to vector struct
+// output: void
 void Vector3_Subtract(struct Vector3 *v1, struct Vector3 *v2);
+
+// scales the vector
+// input: a pointer to a vector struct, a double factor
+// output: void
 void Vector3_Scale(struct Vector3 *v, double factor);
+
+// changes the vector into a unit vector
+// input: a pointer to a vector struct
+// output: void
 void Vector3_Normalize(struct Vector3 *v);
+
 //Return the square of module length of v
+// input: a pointer to a vector
+// output: the square of the magniture of the vector as a double 
 double Vector3_MagnitudeSq(struct Vector3 *v);
 
 //Return the module length of v
+// input: a pointer to a vector
+// output: the magniture of the vector as a double
 double Vector3_Magnitude(struct Vector3 *v);
 
 //Return the square of distance between v1 and v2
+// input: two pointers to vectors
+// output: the square of the distance of two vectors vector as a double 
 double Vector3_DistanceSq3D(struct Vector3 *v1, struct Vector3 *v2);
 
 //Return the distance between v1 and v2
+// input: two pointers to vectors
+// output: the distance of two vectors vector as a double 
 double Vector3_Distance3D(struct Vector3 *v1, struct Vector3 *v2);
 
 //Return the result of cross product of v1 and v2, only considering the x and y
@@ -46,18 +77,28 @@ double Vector3_CrossProduct2D(struct Vector3 *v1, struct Vector3 *v2);
 void Vector3_CrossProduct3D(struct Vector3 *v1, struct Vector3 *v2, struct Vector3 *result);
 
 //Return the result of dot product of v1 and v2
+// input: two pointers to vectors
+// output: the dot product of the two vectors as a double
 double Vector3_DotProduct3D(struct Vector3 *v1, struct Vector3 *v2);
 
 //Return the result of dot product of v1 and v2, only considering the x and y
+// input: two pointers to a vector
+// output: the square of the magniture of the vector as a double 
 double Vector3_DistanceSq2D(struct Vector3 *v1, struct Vector3 *v2);
 
 //Return the distance between v1 and v2, only considering the x and y
+// input: two pointers to a vector
+// output: the magniture of the vector as a double
 double Vector3_Distance2D(struct Vector3 *v1, struct Vector3 *v2);
 
 //Return True <=> the value of v1 equal to that of v2
+// input: two pointers to vectors
+// output: an integer similar to a boolean variable representing whether the vectors are equal
 int Vector3_Equal(struct Vector3 *v1, struct Vector3 *v2);
 
 //Multiply the value of each x, y, z
+// input: one vector pointer that is multiplied, another vector pointer used to multiply with the first one
+// output: void
 void Vector3_Multiply(struct Vector3 *v1, struct Vector3 *v2);
 
 #define OP_VECTOR
